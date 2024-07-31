d7c3183a62ea1bde751b27cf2acbd108cde2dbb7
Praveen-Hapuarachchi <hapup14@gmail.com>
2024-07-31 17:45:32 +0530

    update

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addAdmin, adminLogin, getAdmins } from '../controllers/admin-controller.js';
+import { addAdmin, adminLogin, getAdminById, getAdmins } from '../controllers/admin-controller.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post("/singup",addAdmin);
 adminRouter.post("/login",adminLogin);
 adminRouter.get("/",getAdmins);
+adminRouter.get("/:d",getAdminById);
 
 export default adminRouter;
